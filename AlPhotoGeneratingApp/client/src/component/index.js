be7886c68299d7e01a28780ae34abf79ce44e867
mdyousufhossain <yousafhossain3@gmail.com>
2023-02-14 20:09:12 +0600

    first part of this project is done

--- AlPhotoGeneratingApp/client/src/component/index.js
+++ AlPhotoGeneratingApp/client/src/component/index.js
@@ -1,7 +1,13 @@
 import Home from "./page/Home";
 import CreatePost from "./page/CreatePost";
+import Card from "./Card";
+import FormField from "./FormField";
+import Loader from "./Loader";
 
 export {
     Home as HomePage,
-    CreatePost as CreatePostPage 
+    CreatePost as CreatePostPage ,
+    Card,
+    FormField,
+    Loader,
 }
