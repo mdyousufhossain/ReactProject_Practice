5c661ae063b175f23b40bc7e499be1bf88c49fe0
mdyousufhossain <yousafhossain3@gmail.com>
2023-02-14 14:40:45 +0600

    working on new project Ai pic generating project

---
+++ AlPhotoGeneratingApp/client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { HomePage,CreatePostPage } from './component'
+import { logo } from './assets'
+
+const App = () => {
+  return (
+    <h1 className='text-8xl'> hello mother fucker</h1>
+  )
+}
+
+export default App
---
+++ AlPhotoGeneratingApp/client/src/component/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import CreatePost from "./CreatePost";
+
+export {
+    Home as HomePage,
+    CreatePost as CreatePostPage 
+}
