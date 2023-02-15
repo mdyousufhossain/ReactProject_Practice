4a965f2b63883481543fa6fc1f1adfeef05eef24
mdyousufhossain <yousafhossain3@gmail.com>
2023-02-15 19:23:43 +0600

    frontend is pretty done

--- AlPhotoGeneratingApp/client/src/component/Header.jsx
+++ AlPhotoGeneratingApp/client/src/component/Header.jsx
@@ -6,9 +6,7 @@ import { Link  } from 'react-router-dom'
   return (
     <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
     <Link to="/">
-      <img src={logo} alt="logo" 
-      className='w-28 object-contain'
-      />
+      <h1 className='txt-primary'>OMG AI </h1>
     </Link>
     <Link to="/create-post"
     className='font-inter font-medium bg-[#6469ff] text-white
