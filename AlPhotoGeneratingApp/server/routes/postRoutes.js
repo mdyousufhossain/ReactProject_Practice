1eeb23c548d11f0d785630f2662f131adffdd008
mdyousufhossain <yousafhossain3@gmail.com>
2023-02-17 11:09:56 +0600

    dalle app , added cloudinary to the database , succefully added photo scema

--- AlPhotoGeneratingApp/server/routes/postRoutes.js
+++ AlPhotoGeneratingApp/server/routes/postRoutes.js
@@ -7,14 +7,27 @@ import Post from '../mongodb/models/post.js'
 dotenv.config();
 
 const router = express.Router();
-
+// adding cloudinary config info
 cloudinary.config({
     cloud_name : process.env.ClOULDINARY_CLOUD_NAME,
     api_key : process.env.ClOULDINARY_API_KEY,
     api_secret : process.env.ClOULDINARY_API_SECRET,
 })
 router.route('/').get(async(req,res) => {
+   try { 
     const { name , prompt , photo } = req.body
+    const photoUrl = await cloudinary.uploader.upload(photo)
+    // creating photo schema 
+    const newPost = await Post.create({
+        name,
+        prompt,
+        photo: photoUrl.url 
+    })
+    // posting data in database 
+    res.status(201).json({success:true, data: newPost});
+    } catch(error){
+        res.status(500).json({success:false, message:error})
+    }
 })
 
 
